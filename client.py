f48062b3cc9516e150d7e019ab5e203c6406778b
fillhse <fillhseuni@gmail.com>
2024-12-13 15:51:41 +0300

    Gomoku-game

---
+++ client.py
@@ -0,0 +1,299 @@
+import requests
+import time
+import os
+import re
+
+class TicTacToeClient:
+    def __init__(self):
+        self.username = None
+        self.game_id = None
+        self.SERVER_URL = None
+
+
+    def slogan_gomoku(self):
+        print('''
+              ____                           _
+             / ___|  ___   _ __ ___    ___  | | __ _   _
+            | |  _  / _ \ | '_ ` _ \  / _ \ | |/ /| | | |
+            | |_| || (_) || | | | | || (_) ||   < | |_| |
+             \____| \___/ |_| |_| |_| \___/ |_|\_\ \__,_|
+
+            ''')
+    def get_server(self):
+        while True:
+            pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.)){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?){1}$'
+            ans_server_url = input('''
+        To play on the official server, Enter word: <server>
+        To play on a local network, Enter word: <local>
+        If you want to play on an unofficial server, Enter IP address of this server
+            command: ''')
+
+            if ans_server_url in ["server", "<server>"]:
+                self.SERVER_URL = "http://localhost:8080"
+                break
+            if ans_server_url in ["local", "<local>"]:
+                self.SERVER_URL = "http://localhost:8080"
+                break
+            if bool(re.match(pattern, ans_server_url)):
+                self.SERVER_URL = ans_server_url
+                break
+            else:
+                print("BAD VALUE")
+
+    def register(self):
+        os.system('cls')
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        response = requests.post(f"{self.SERVER_URL}/register", json={"username": username, "password": password})
+        print(response.json()["message"])
+
+    def login(self):
+        os.system('cls')
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        response = requests.post(f"{self.SERVER_URL}/login", json={"username": username, "password": password})
+        if response.json()["success"]:
+            self.username = username
+        print(response.json()["message"])
+
+    def get_ids_all_games(self):
+        if self.username:
+            response = requests.post(f"{self.SERVER_URL}/get_ids_all_games", json={"username": self.username})
+            if response.json()["success"]:
+                os.system('cls')
+                print(f"Ids_all_games: {response.json()['games']}")
+            else:
+                os.system('cls')
+                print(response.json()["message"])
+        else:
+            os.system('cls')
+            print("Please Login or Register\nIf you're registered, you need to login")
+    def create_game(self):
+        if self.username:
+            response = requests.post(f"{self.SERVER_URL}/create_game", json={"username": self.username})
+            if response.json()["success"]:
+                os.system('cls')
+                print(f"Game created with ID: {response.json()['game_id']}")
+            else:
+                os.system('cls')
+                print(response.json()["message"])
+        else:
+            os.system('cls')
+            print("Please Login or Register\nIf you're registered, you need to login")
+
+    def check_game_history(self):
+        if self.username:
+            os.system('cls')
+            self.game_id = input("Enter the number of the game you are interested in, in which you participated: ")
+            response = requests.post(f"{self.SERVER_URL}/check_game_history", json={"username": self.username, "game_id": self.game_id})
+            if response.json()["success"]:
+
+                self.data = response.json()["data_history"]
+                exit_check=True
+                while exit_check:
+                    print("1. Write the number of the move you are interested in (starts from 1)\n Write <exit> to return to the menu")
+                    print(f'Total moves: {len(self.data)//23}')
+                    ans=input("Enter: ")
+                    os.system('cls')
+                    print(f'MOVE: {ans}')
+                    if ans in ["exit","<exit>"]:
+                        break
+                    try:
+                        i=int(ans)-1
+                        for line in range(23*i,23*(i+1)):
+                            print(self.data[line][:-1])
+                    except:
+                        print("Invalid Value")
+
+            else:
+                os.system('cls')
+                print(response.json()["message"])
+        else:
+            os.system('cls')
+            print("Please Login or Register\nIf you're registered, you need to login")
+
+
+
+
+
+    def join_game(self):
+        if self.username:
+            self.game_id = int(input("Enter game ID to join: "))
+            os.system('cls')
+            response = requests.post(f"{self.SERVER_URL}/join_game", json={"username": self.username, "game_id": self.game_id})
+            print(response.json()["message"])
+            c_wainting = 1
+            while True:
+                print(f'Wait 10 seconds for second user')
+                time.sleep(10)
+                response = requests.post(f"{self.SERVER_URL}/c_players_in_game", json={"game_id": self.game_id})
+                data = response.json()
+                if data["success"]:
+                    if data["turn"] == self.username:
+                        os.system('cls')
+                        print(f'You and {[el for el in data["players"] if el != self.username][0]} in the game')
+                        print("Your turn\nLet's make  move")
+                    else:
+                        os.system('cls')
+                        print(f'You and {[el for el in data["players"] if el != self.username][0]} in the game')
+                        print("Not your turn\nLet's wait opponent's move")
+                        c_wainting = 1
+                        while True:
+                            print(f'Wait {2.5*c_wainting} seconds for second user')
+                            time.sleep(2.5*c_wainting)
+                            response = requests.post(f"{self.SERVER_URL}/wait_move_second", json={"username": self.username, "game_id": self.game_id})
+                            data = response.json()
+                            if data["success"]:
+                                os.system('cls')
+                                print(f'Your opponent has made a move.')
+                                break
+                            else:
+                                if c_wainting % 6 == 0:
+                                    os.system('cls')
+                                    want_exit = input("Second user hasn't made a move yet\nIf you want to continue waiting write <cont>\nIf you wait exit write <exit>\n command: ")
+                                    if want_exit in ["<cont>", "cont"]:
+
+                                        continue
+                                    elif want_exit in ["<exit>", "exit"]:
+                                        os.system('cls')
+                                        break
+                                    else:
+                                        print("Write only <cont> or <exit>")
+                                else:
+                                    c_wainting += 1
+                    break
+                elif c_wainting % 6 == 0:
+                    os.system('cls')
+                    want_exit = input("Second user not connected yet\nIf you want to continue waiting write <cont>\nIf you wait exit write <exit>\n command: ")
+                    if want_exit in ["<cont>", "cont"]:
+
+                        continue
+                    elif want_exit in ["<exit>", "exit"]:
+                        os.system('cls')
+                        break
+                    else:
+                        print("Write only <cont> or <exit>")
+                else:
+                    c_wainting += 1
+
+        else:
+            os.system('cls')
+            print("Please Login or Register\nIf you're registered, you need to login")
+
+
+    def view_board(self):
+        response = requests.post(f"{self.SERVER_URL}/view_board", json={"username": self.username, "game_id": self.game_id})
+        data = response.json()
+        if data["success"]:
+            os.system('cls')
+            print(f'{" "*4} {"".join([f"{str(int_columm):3}" for int_columm in range(1,20)])}')
+            for row in range(len(data["board"])):
+                print(f'{row+1:3}| {"  ".join(data["board"][row])}')
+            return "board_is_shown"
+        else:
+            return data["message"]
+
+    def make_move(self):
+        if self.game_id and self.username:
+            x=None
+            y=None
+            res_view=TicTacToeClient.view_board(self)
+            if res_view == "board_is_shown":
+                try:
+                    x = int(input("Enter row (1-19): "))
+                    y = int(input("Enter column (1-19): "))
+                    response = requests.post(f"{self.SERVER_URL}/make_move", json={"username": self.username, "game_id": self.game_id, "x": x-1, "y": y-1})
+                except:
+                    response = requests.post(f"{self.SERVER_URL}/make_move", json={"username": self.username, "game_id": self.game_id, "x": x, "y": y})
+            else:
+                response = requests.post(f"{self.SERVER_URL}/make_move", json={"username": self.username, "game_id": self.game_id, "x": x, "y": y})
+            data = response.json()
+            if data["success"]:
+                os.system('cls')
+                print(f'{" "*4} {"".join([f"{str(int_columm):3}" for int_columm in range(1,20)])}')
+                for row in range(len(data["board"])):
+                    print(f'{row+1:3}| {"  ".join(data["board"][row])}')
+                if res_view == "board_is_shown":
+                    print("Move successful!")
+                if data["winner"]:
+                    for _ in range(3):
+                        print(f"Winner: {data['winner']}")
+                else:
+                    c_wainting = 1
+                    while True:
+                        print(f'Wait {2.5*(c_wainting)} seconds for second user')
+                        time.sleep(2.5*c_wainting)
+                        response = requests.post(f"{self.SERVER_URL}/wait_move_second", json={"username": self.username, "game_id": self.game_id})
+                        data = response.json()
+                        if data["success"]:
+                            if data["winner"]:
+                                os.system('cls')
+                                print(f'{" " * 4} {"".join([f"{str(int_columm):3}" for int_columm in range(1, 20)])}')
+                                for row in range(len(data["board"])):
+                                    print(f'{row + 1:3}| {"  ".join(data["board"][row])}')
+                                for _ in range(3):
+                                    print(f"Winner: {data['winner']}")
+
+                            else:
+                                os.system('cls')
+                                print(f'Your opponent has made a move.')
+                            break
+                        else:
+                            if c_wainting % 6 == 0:
+                                want_exit = input("Second user hasn't made a move yet\nIf you want to continue waiting write <cont>\nIf you wait exit write <exit>\n command: ")
+                                if want_exit in ["<cont>", "cont"]:
+                                    os.system('cls')
+                                    continue
+                                if want_exit in ["<exit>", "exit"]:
+                                    os.system('cls')
+                                    break
+                            c_wainting += 1
+
+
+            else:
+                os.system('cls')
+                print(data["message"])
+        elif self.username:
+            os.system('cls')
+            print("Please join game (point 4)")
+        else:
+            os.system('cls')
+            print("Please Login or Register\nIf you're registered, you need to login")
+if __name__ == "__main__":
+    client = TicTacToeClient()
+
+    os.system('cls')
+
+    client.get_server()
+
+    os.system('cls')
+
+    client.slogan_gomoku()
+
+    while True:
+        try:
+            print("\n1. Register\n2. Login\n3. Create Game\n4. Join Game\n5. Make Move\n6. Check game history\n7. Get ids all games\n8. Exit")
+            choice = input("Select an option: ")
+            if choice == "1":
+                client.register()
+            elif choice == "2":
+                client.login()
+            elif choice == "3":
+                client.create_game()
+            elif choice == "4":
+                client.join_game()
+            elif choice == "5":
+                client.make_move()
+            elif choice == "6":
+                client.check_game_history()
+            elif choice == "7":
+                client.get_ids_all_games()
+            elif choice == "8":
+                break
+            else:
+                print("Invalid choice. Try again.")
+        except ConnectionError:
+            print("Проверьте доступ к сети\nИли обратитесь к администратору сервера")
+        except Exception as e:
+            print(f"{type(e).__name__}\nError: {e}")
+
---
+++ server.py
@@ -0,0 +1,256 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import os
+
+PORT = 8080
+
+GAMES = {}
+DATA_GAME = {}
+
+# Загружаем игроков из файла
+try:
+    with open("data_game.json", "r") as f:
+        DATA_GAME = json.load(f)
+except FileNotFoundError:
+    DATA_GAME = {"total_id_game": 0}
+
+# Проверяем или создаем папку GAMES_HISTORY
+if not os.path.isdir("GAMES_HISTORY"):
+    os.makedirs("GAMES_HISTORY")
+
+
+def save_data_game():
+    with open("data_game.json", "w") as f:
+        json.dump(DATA_GAME, f)
+
+
+class GameServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+
+        if self.path == "/register":
+            self.register_player(data)
+        elif self.path == "/login":
+            self.login_player(data)
+        elif self.path == "/create_game":
+            self.create_game(data)
+        elif self.path == "/join_game":
+            self.join_game(data)
+        elif self.path == "/c_players_in_game":
+            self.c_players_in_game(data)
+        elif self.path == "/make_move":
+            self.make_move(data)
+        elif self.path == "/wait_move_second":
+            self.wait_move_second(data)
+        elif self.path == "/view_board":
+            self.view_board(data)
+        elif self.path == "/save_game_history":
+            self.save_game_history(data)
+        elif self.path == "/check_game_history":
+            self.check_game_history(data)
+        elif self.path == "/get_ids_all_games":
+            self.get_ids_all_games(data)
+        else:
+            self.send_error(404, "Endpoint not found")
+
+    def get_ids_all_games(self,data):
+        username = data.get("username")
+        if username in DATA_GAME:
+            self.send_json_response({"success": True, "games": DATA_GAME[username]["games"]})
+        else:
+            self.send_json_response({"success": False, "message": "Invalid username"})
+
+
+
+
+    def save_game_history(self,GAMES,game_id):
+        for user in GAMES[game_id]["players"]:
+            with open(f'GAMES_HISTORY/Game_{game_id}_{user}.txt', "a+", encoding="utf-8") as f:
+                f.write(f'PLAYERS: {GAMES[game_id]["players"][0]} - ○ ; {GAMES[game_id]["players"][1]} - ●\n')
+                f.write(f'MADE A MOVE: {[el for el in GAMES[game_id]["players"] if el != GAMES[game_id]["turn"]][0]}\n')
+                f.write(f'WINNER: {GAMES[game_id]["winner"]}\n')
+                f.write(f'{" " * 4} {"".join([f"{str(int_columm):3}" for int_columm in range(1, 20)])}\n')
+                for row in range(len(GAMES[game_id]["board"])):
+                    f.write(f'{row + 1:3}| {"  ".join(GAMES[game_id]["board"][row])}\n')
+                f.close()
+
+    def check_game_history(self,data):
+        game_id = data.get("game_id")
+        username = data.get("username")
+
+        try:
+            with open(f'GAMES_HISTORY/Game_{game_id}_{username}.txt', 'r', encoding="utf-8") as file:
+                data = file.readlines()
+                file.close()
+                self.send_json_response({"success": True, "data_history": data})
+                return
+        except IOError as e:
+            print(e)
+            self.send_json_response({"success": False, "message": u'The file could not be opened/double-check the data'})
+            return
+
+
+
+
+
+    def send_json_response(self, response):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode("utf-8"))
+
+    def register_player(self, data):
+        username = data.get("username")
+        password = data.get("password")
+
+        if username in DATA_GAME:
+            self.send_json_response({"success": False, "message": "User already exists."})
+            return
+        if len(username)<=3 or len(password)<=3:
+            self.send_json_response({"success": False, "message": "Login and Password lengths must be more than 3"})
+            return
+
+        DATA_GAME[username] = {"password": password, "games": []}
+        save_data_game()
+        self.send_json_response({"success": True, "message": "User registered successfully."})
+
+    def login_player(self, data):
+        username = data.get("username")
+        password = data.get("password")
+
+        if username not in DATA_GAME or DATA_GAME[username]["password"] != password:
+            self.send_json_response({"success": False, "message": "Invalid username or password."})
+            return
+
+        self.send_json_response({"success": True, "message": "Login successful."})
+
+    def create_game(self, data):
+        DATA_GAME["total_id_game"] += 1
+        game_id = DATA_GAME["total_id_game"]
+        username = data.get("username")
+
+        GAMES[game_id] = {
+            "players": [],
+            "board": [["~"] * 19 for _ in range(19)],
+            "turn": "",
+            "winner": None
+        }
+
+
+        self.send_json_response({"success": True, "game_id": game_id})
+
+    def join_game(self, data):
+        game_id = data.get("game_id")
+        username = data.get("username")
+
+        if username in GAMES[game_id]["players"]:
+            self.send_json_response({"success": True, "message": f'Already join game {game_id}.'})
+            return
+
+        if game_id not in GAMES or len(GAMES[game_id]["players"]) >= 2:
+            self.send_json_response({"success": False, "message": f'Cannot join game {game_id}.'})
+            return
+
+        GAMES[game_id]["players"].append(username)
+        DATA_GAME[username]["games"].append(game_id)
+        if GAMES[game_id]["turn"] == "":
+            GAMES[game_id]["turn"] = username
+        save_data_game()
+        self.send_json_response({"success": True, "message": f'Joined game {game_id}.'})
+    def c_players_in_game(self,data):
+        game_id = data.get("game_id")
+        if len(GAMES[game_id]["players"]) < 2:
+            self.send_json_response({"success": False, "players": GAMES[game_id]["players"], "turn": GAMES[game_id]["turn"]})
+            return
+        else:
+            self.send_json_response({"success": True, "players": GAMES[game_id]["players"], "turn": GAMES[game_id]["turn"]})
+            return
+
+
+
+    def view_board(self, data):
+        game_id = data.get("game_id")
+
+        if game_id not in GAMES:
+            self.send_json_response({"success": False, "message": "Invalid game.", "board": GAMES[game_id]["board"]})
+            return
+        if GAMES[game_id]["winner"]:
+            self.send_json_response({"success": False, "message": "Have winner.", "board": GAMES[game_id]["board"]})
+            return
+
+        self.send_json_response({"success": True, "board": GAMES[game_id]["board"]})
+        return
+
+    def wait_move_second(self,data):
+        username = data.get("username")
+        game_id = data.get("game_id")
+        if GAMES[game_id]["turn"] == username:
+            self.send_json_response({"success": True, "winner": GAMES[game_id]["winner"], "board": GAMES[game_id]["board"]})
+            return
+        else:
+            self.send_json_response({"success": False})
+            return
+
+    def make_move(self, data):
+        game_id = data.get("game_id")
+        username = data.get("username")
+        x, y = data.get("x"), data.get("y")
+        game = GAMES[game_id]
+
+
+        if game_id not in GAMES:
+            self.send_json_response({"success": False, "message": "Invalid game."})
+            return
+        if GAMES[game_id]["winner"]:
+
+            self.send_json_response({"success": True, "message": "Have winner.", "board": game["board"], "winner": game["winner"]})
+            return
+        if x not in ([i for i in range(0,19)]) or y not in ([i for i in range(0,19)]):
+            self.send_json_response({"success": False, "message": "Invalid row or collumm."})
+            return
+
+        if game["turn"] != username:
+            self.send_json_response({"success": False, "message": "Not your turn."})
+            return
+
+        if game["board"][x][y] != "~":
+            self.send_json_response({"success": False, "message": "Cell already occupied."})
+            return
+
+        game["board"][x][y] = "○" if game["players"].index(username) == 0 else "●"
+        game["turn"] = game["players"][1] if game["turn"] == game["players"][0] else game["players"][0]
+
+        # Проверка победителя
+        if self.check_winner(game["board"], x, y):
+            game["winner"] = username
+
+        self.save_game_history(GAMES,game_id)
+        self.send_json_response({"success": True, "board": game["board"], "winner": game["winner"]})
+
+    def check_winner(self, board, x, y):
+        def check_direction(dx, dy):
+            count = 1
+            for direction in (-1, 1):
+                nx, ny = x + direction * dx, y + direction * dy
+                while 0 <= nx < 19 and 0 <= ny < 19 and board[nx][ny] == board[x][y]:
+                    count += 1
+                    nx += direction * dx
+                    ny += direction * dy
+                if count >= 5:
+                    return True
+            return False
+
+        return (check_direction(1, 0) or  # Horizontal
+                check_direction(0, 1) or  # Vertical
+                check_direction(1, 1) or  # Diagonal down-right
+                check_direction(1, -1) or # Diagonal down-left
+                check_direction(-1, 1) or # Diagonal up-right
+                check_direction(-1, -1) #Diagonal up-left
+                )
+if __name__ == "__main__":
+    server_address=input("Input ip_address server\nFor local game input word: localhost\n    command: ")
+    server = HTTPServer((server_address, PORT), GameServer)
+    print(f"Server running on port {PORT}...")
+    server.serve_forever()
