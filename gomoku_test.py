203ee3348421eb98ecbf8e8880c841ae7be445d3
fillhseuni <fillhseuni@gmail.com>
2024-12-17 22:07:30 +0300

    Добавлены тесты get_server, get_ids_all_games для gomoku, check_winner для gomoku_server

--- gomoku_test.py
+++ gomoku_test.py
@@ -1,13 +1,36 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from gomoku import GomokuClient
-
+import io
+import sys
 
 class TestGomokuClient(unittest.TestCase):
 
     def setUp(self):
         self.client = GomokuClient()
+        self.client.username = "user1"
         self.client.SERVER_URL = "https://localhost:8443"
+        self.board = [
+                ["●", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+            ]
 
     @patch('gomoku.requests.post')
     def test_login_success(self, mock_post):
@@ -39,23 +62,50 @@ class TestGomokuClient(unittest.TestCase):
         result = self.client.view_board()
         self.assertEqual(result, "board_is_shown")
 
-    @patch('gomoku.requests.post')
-    def test_get_server_success_server(self, mock_post):
+    def test_get_server_success_server(self):
         with patch('builtins.input', side_effect=["server"]):
             self.client.get_server()
         self.assertEqual(self.client.SERVER_URL, "https://109.196.98.96:8443")
 
-    @patch('gomoku.requests.post')
-    def test_get_server_success_local(self, mock_post):
+    def test_get_server_success_local(self):
         with patch('builtins.input', side_effect=["local"]):
             self.client.get_server()
         self.assertEqual(self.client.SERVER_URL, "https://localhost:8443")
 
-    @patch('gomoku.requests.post')
-    def test_get_server_success_anyip(self, mock_post):
+    def test_get_server_success_anyip(self):
         with patch('builtins.input', side_effect=["196.128.64.32"]):
             self.client.get_server()
         self.assertEqual(self.client.SERVER_URL, "https://196.128.64.32:8443")
+
+    def test_get_server_failure(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        with patch('builtins.input', side_effect=["0.0.0..0"]):
+            self.client.get_server() 
+        self.assertEqual(captured_output.getvalue(), '''BAD VALUE\n''')
+
+
+    @patch('gomoku.requests.post')
+    def test_get_ids_all_games_success(self, mock_post):
+        mock_post.return_value.json.return_value = {
+            "success": True, "games": list(range(10)) 
+        }
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        self.client.get_ids_all_games() 
+        self.assertEqual(captured_output.getvalue(), f"Ids_all_games: {list(range(10))}\n")
+
+    @patch('gomoku.requests.post')
+    def test_get_ids_all_games_invalid(self, mock_post):
+        mock_post.return_value.json.return_value = {
+            "success": False, "message": "Invalid username" 
+        }
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        self.client.get_ids_all_games() 
+        self.assertEqual(captured_output.getvalue(), f"Invalid username\n")
+
+    
 
 
 if __name__ == '__main__':
